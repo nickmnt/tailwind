6eee9f21537ebe607a2ec72a320de347c72d0559
Devin <mtaghavinima@gmail.com>
2023-03-01 20:13:40 +0330

    Customizing the tailwind design system

--- introduction/tailwind.config.js
+++ introduction/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-blue": "#1992d4",
+      },
+      spacing: {
+        72: "18rem",
+      },
+    },
   },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "active"],
