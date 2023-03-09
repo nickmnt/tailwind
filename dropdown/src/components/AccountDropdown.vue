1b1ab1443f4c83cb423cc37f488686ffeefe56df
Devin <mtaghavinima@gmail.com>
2023-03-09 21:39:02 +0330

    Dropdown implemented

---
+++ dropdown/src/components/AccountDropdown.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button class="block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
+      <img class="h-full w-full object-cover" src="/img/avatar.avif" alt="Your avatar">
+    </button>
+    <div class="mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
+      <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account settings</a>
+      <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Support</a>
+      <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sign out</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
