c319014e408e32592d0ac4886667fdf643ca6dad
Devin <mtaghavinima@gmail.com>
2023-03-09 19:42:37 +0330

    Making the navbar responsive

--- navbar/src/components/Navbar.vue
+++ navbar/src/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
-  <header class="bg-gray-900">
-    <div class="flex items-center justify-between px-4 py-3">
+  <header class="bg-gray-900 sm:flex sm:justify-between sm:px-4 sm:py-3 sm:items-center">
+    <div class="flex items-center justify-between px-4 py-3 sm:p-0">
       <div class="">
         <img class="h-8" src="/img/logo-inverted.svg" alt="Workcation">
       </div>
       <div>
-        <button @click="isOpen = !isOpen" type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+        <button @click="isOpen = !isOpen" type="button" class="sm:hidden block text-gray-500 hover:text-white focus:text-white focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
               <path v-else fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
@@ -13,10 +13,10 @@
         </button>
       </div>
     </div>
-    <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4">
+    <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
       <a href="#" class="block text-white font-semibold hover:bg-gray-800 rounded px-2 py-1">List your property</a>
-      <a href="#" class="block text-white font-semibold hover:bg-gray-800 rounded px-2 py-1 mt-1">Trips</a>
-      <a href="#" class="block text-white font-semibold hover:bg-gray-800 rounded px-2 py-1 mt-1">Messages</a>
+      <a href="#" class="block text-white font-semibold hover:bg-gray-800 rounded px-2 py-1 mt-1 sm:mt-0 sm:ml-2">Trips</a>
+      <a href="#" class="block text-white font-semibold hover:bg-gray-800 rounded px-2 py-1 mt-1 sm:mt-0 sm:ml-2">Messages</a>
     </div>
   </header>
 </template>
